cbe92dda5b0465b55a78405f7dbbc380b2f259d9
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-02-09 19:52:01 +0000

    philosophy page wip

---
+++ client/momentum.js
@@ -0,0 +1,56 @@
+var duration = 500;
+
+Momentum.registerPlugin("slide-fade", function(options){
+  return {
+    insertElement: function(node, next, done){
+      var $node = $(node);
+
+      $node.hide()
+        .insertBefore(next)
+        .velocity("fadeIn",{
+          duration: duration,
+          delay: duration
+        });
+    },
+    removeElement: function(node, done){
+      var $node = $(node),
+          d = ($node.is(".prev")) ? -1 : 1;
+
+      $node.velocity({
+        translateX: $node.offset().left + $node.outerWidth() * d,
+        opacity: 0
+      }, {
+        easing: "ease-in-out",
+        duration: duration,
+        complete: function(){
+          $node.remove();
+          done();
+        }
+      });
+    }
+  };
+});
+
+Momentum.registerPlugin("fade-text", function(options){
+  return {
+    insertElement: function(node,next,done){
+      $(node).hide().insertAfter(next).velocity("fadeIn", {
+        duration: duration,
+
+        complete: function(){
+          done();
+        }
+      });
+    },
+    removeElement: function(node, done){
+      var $node = $(node);
+      $node.velocity("fadeOut", {
+        duration: duration,
+        complete: function(){
+          $node.remove();
+          done();
+        }
+      });
+    }
+  };
+});
--- client/templates/philosophy-page.js
+++ client/templates/philosophy-page.js
@@ -1,16 +1,59 @@
-var slides = [
-  "simplicity",
-  "universality",
-  "modularity",
-  "nonDiscrimination"
-], currentSlide = slides[0];
+var mod = function(i,n){ return ((i % n)+n)%n; }; // built-in js mod sucks
+
+var slides = [{
+  background: "simplicityBackground",
+  content: "simplicityContent",
+  title: "Simplicity"
+},{
+  background: "universalityBackground",
+  content: "universalityContent",
+  title: "Universality"
+},{
+  background: "modularityBackground",
+  content: "modularityContent",
+  title: "Modularity"
+},{
+  background: "nonDiscriminationBackground",
+  content: "nonDiscriminationContent",
+  title: "Non-Discrimination"
+}], switchTo;
+
+Template.philosophyPage.created = function(){
+  Session.set("philosophyPage_slideIndex", 0);
+};
+
+Template.philosophyPage.rendered = function(){
+  
+};
 
 Template.philosophyPage.helpers({
   slide: function(){
-    console.log(currentSlide);
-    return {
-      background: currentSlide + "Background",
-      content: currentSlide + "Content"
-    };
+    var i = Session.get("philosophyPage_slideIndex");
+    return _.extend(slides[i], {
+      prev: slides[mod(i-1, slides.length)].title,
+      next: slides[mod(i+1, slides.length)].title,
+      switchTo: switchTo
+    });
+  },
+  philosophySlideHeadline: function(){
+    var i = Session.get("philosophyPage_slideIndex");
+  }
+});
+
+Template.philosophyPage.events({
+  "click .slide-control": function(e, tmpl){
+    var i = Session.get("philosophyPage_slideIndex"),
+        n = slides.length;
+        
+
+    if($(e.currentTarget).is(".left")){
+      Session.set("philosophyPage_slideIndex", mod(i-1, n));
+      switchTo = "prev";
+    } else if($(e.currentTarget).is(".right")) {
+      Session.set("philosophyPage_slideIndex", mod(i+1, n));
+      switchTo = "next";
+    } else {
+      throw new Error("slide control direction unknown!", e.currentTarget);
+    }
   }
 });
