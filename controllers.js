cf45070cf3176fab5e2deb9f51116b1a72da1603
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-02-16 13:02:44 +0000

    moved news to the home page

--- controllers.js
+++ controllers.js
@@ -14,12 +14,15 @@ HomeController = AppController.extend({
   subscriptions: function(){
     this.subscribe("meetups");
     this.subscribe("youtubeVideos");
+    this.subscribe("feed_entries");
   },
   data: function(){
     return {
       featuredMeetups: Meetups.find({featured: true}),
       meetups: Meetups.find(),
-      youtubeVideos: YoutubeVideos.find({},{limit:4, sort: {pubDate: -1}})
+      youtubeVideos: YoutubeVideos.find({},{limit:4, sort: {pubDate: -1}}),
+      blogFeed: FeedEntries.find({feed_category: "Blog"}, {limit: 3, sort: {pubdate: -1}}),
+      tweets: FeedEntries.find({feed_category: "Twitter"}, {limit: 5, sort: {pubdate: -1}})
     };
   },
   action: function(){
@@ -45,21 +48,6 @@ AdminSignInController = AppController.extend({
   },
   action: function(){
     this.render("adminSignInPage");
-  }
-});
-
-NewsController = AppController.extend({
-  subscriptions: function(){
-    Meteor.subscribe("feed_entries");
-  },
-  data: function(){
-    return {
-      blogFeed: FeedEntries.find({feed_category: "Blog"}, {limit: 3, sort: {pubdate: -1}}),
-      tweets: FeedEntries.find({feed_category: "Twitter"}, {limit: 5, sort: {pubdate: -1}})
-    };
-  },
-  action: function(){
-    this.render("newsPage");
   }
 });
 
