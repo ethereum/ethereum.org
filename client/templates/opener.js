689f27196d7b4fe97bb3b7371c7c6524a48140ff
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-02-25 19:50:25 +0000

    restyling the home page wip

--- client/templates/opener.js
+++
@@ -1,9 +0,0 @@
-var stopScroll;
-
-Template.opener.rendered = function(){
-  stopScroll = UI.autoscroll(this.$(".tweets-container .scroll"));
-};
-
-Template.opener.destroyed = function(){
-  stopScroll();
-};
---
+++ client/templates/scrolling-tweets.js
@@ -0,0 +1,9 @@
+var stopScroll;
+
+Template.scrollingTweets.rendered = function(){
+  stopScroll = UI.autoscroll(this.$(".tweets-container .scroll"));
+};
+
+Template.scrollingTweets.destroyed = function(){
+  stopScroll();
+};
