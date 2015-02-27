6e416429a33575fbf63af6475e245e1df3f1a341
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-02-27 17:04:10 +0000

    timing fixes for the kcud

--- client/templates/duck.js
+++ client/templates/duck.js
@@ -1,14 +1,20 @@
-Template.duck.rendered = function(){
-  $(document).mousemove(_.throttle(function(){
-    var x = Math.random();
-    if(x < 0.025){
-      Session.set("engage-the-duck", true);
+var toDuckOrNotToDuck = _.throttle(function(){
+  var x = Math.random();
+  if(x < 0.025){
+    Session.set("engage-the-duck", true);
 
-      setTimeout(function(){
-        Session.set("engage-the-duck", false);
-      }, 400);
-    }
-  }, 10000));
+    setTimeout(function(){
+      Session.set("engage-the-duck", false);
+    }, 400);
+  }
+}, 10000, {leading: false});
+
+Template.duck.rendered = function(){
+  $(document).on("mousemove",toDuckOrNotToDuck);
+};
+
+Template.duck.destroyed = function(){
+  $(document).off("mousemove",toDuckOrNotToDuck);
 };
 
 Template.duck.helpers({
