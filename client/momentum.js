0f460691f908a02c7ab366ca3f40935ed8fc1e87
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-03-02 10:48:16 +0000

    removing used tagline li's

--- client/momentum.js
+++ client/momentum.js
@@ -9,16 +9,18 @@ Momentum.registerPlugin('fade2', function(options) {
         .velocity('fadeIn',{
           duration: options.duration || 200
         });
-          
+      
     },
     removeElement: function(node) {
       $(node).velocity('fadeOut', {
-        duration: options.duration || 200
-      },function() {
-        $(this).remove();
+        duration: options.duration || 200,
+        complete: function() {
+          console.log($(this));
+          $(this).remove();
+        }
       });
-          
-    }
       
+    }
+    
   }
 });
