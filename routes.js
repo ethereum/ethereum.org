af45bf5a5d4f1bafaeb92f6a24d13e07dd4960d4
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-03-11 11:27:34 +0000

    w/y paper link redirects

--- routes.js
+++ routes.js
@@ -21,3 +21,11 @@ Router.route("/ether");
 Router.route("/company-structure");
 
 Router.route("/licensing");
+
+Router.route("/pdfs/EthereumWhitePaper.pdf", function(){
+  window.location = "https://github.com/ethereum/wiki/wiki/White-Paper";
+});
+
+Router.route("/pdfs/EthereumYellowPaper.pdf", function(){
+  window.location = "http://gavwood.com/Paper.pdf";
+});
