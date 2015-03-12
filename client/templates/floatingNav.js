82281efe75edcf953e0d803a21afc003b4fc706b
KKudryavtsev <konstantin.k.ed@gmail.com>
2015-03-12 16:01:18 +0000

    reenabled hashbangs in the floating nav

--- client/templates/floatingNav.js
+++ client/templates/floatingNav.js
@@ -9,8 +9,6 @@ Template.floatingNav.destroyed = function(){
 
 Template.floatingNav.events({
   "click a": function(e, tmpl){
-    e.preventDefault();
-
     $($(e.currentTarget).attr("href")).velocity("scroll", {
       duration: 400,
       easing: "ease-out",
